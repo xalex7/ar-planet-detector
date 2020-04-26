366a41eca2ab1761c99d5bb4edb9446cb32e9ec2
Fady Eid <fadyeid7@icloud.com>
2020-04-25 19:52:43 -0600

    Initial Commit

---
+++ Planet Detector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planet Detector
+//
+//  Created by Fady Eid on 4/25/20.
+//  Copyright © 2020 Fady Eid. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
